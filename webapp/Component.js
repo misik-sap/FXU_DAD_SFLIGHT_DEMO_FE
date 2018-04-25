bcc9fd6cfea611641de02b8f15a0174488cb46a5
I335861 <stefan.misik@sap.com>
2018-04-25 08:01:55 +0000

    FXU DAD Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("FXU_DAD.FXU_DAD_SFLIGHT_DEMO.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("FXU_DAD.FXU_DAD_SFLIGHT_DEMO.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
